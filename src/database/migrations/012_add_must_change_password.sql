953936c09a7babbec322c0efdba615e7d8191638
Luiz Neto <liassuo@gmail.com>
2026-03-05 14:28:48 -0300

    fix reset de senha, delete sql

--- src/database/migrations/012_add_must_change_password.sql
+++
@@ -1,3 +0,0 @@
--- Add must_change_password flag to profiles
--- When true, the user must change their password before accessing the system
-ALTER TABLE profiles ADD COLUMN must_change_password BOOLEAN NOT NULL DEFAULT false;
